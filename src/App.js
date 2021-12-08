ce36c3413e89ccd0da87500064803938c06e86bc
Jagadish Pemme <mrjagadish1211@gmail.com>
2021-12-08 22:14:09 +0530

    To do list basic structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Taskcard from './Components/Task_card';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="container">
+    <h1 className= "heading">
+      To-Do List
+    </h1>
+    <input classname="task" type="text" placeholder="Add to list..">
+    
+    </input>
+    <button type="submit" name="Add_task">
+      Add task
+    </button>
+    <Taskcard />
+   </div>
   );
 }
 
---
+++ src/Components/Task_card.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+function Taskcard(){
+    return(
+        <div>
+            <div>
+                This should display the task added.
+            </div>
+            <div>
+                E
+            </div>
+            <div>
+                D
+            </div>
+        </div>
+    )
+}
+
+export default Taskcard;
